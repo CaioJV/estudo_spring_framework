b2e48a839f06beea776eb893cb17007f5f86bbd7
administrador <administrador@eq20206299am.AMRJRJ.AD>
2023-09-13 15:16:23 -0300

    Primeira Versão

---
+++ cursoFramework/src/main/java/br/com/estudo_framework/cursoFramework/CursoFrameworkApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudo_framework.cursoFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoFrameworkApplication.class, args);
+	}
+
+}
---
+++ cursoFramework/src/test/java/br/com/estudo_framework/cursoFramework/CursoFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudo_framework.cursoFramework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
