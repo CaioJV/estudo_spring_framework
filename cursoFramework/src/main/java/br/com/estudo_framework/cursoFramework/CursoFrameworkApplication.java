af6ca77a1e610d2f2766fbcd56a336c6d35d6c76
administrador <administrador@eq20206299am.AMRJRJ.AD>
2023-09-14 08:23:25 -0300

    Adicionando a interface CommandLineRunner

--- cursoFramework/src/main/java/br/com/estudo_framework/cursoFramework/CursoFrameworkApplication.java
+++ cursoFramework/src/main/java/br/com/estudo_framework/cursoFramework/CursoFrameworkApplication.java
@@ -1,13 +1,19 @@
 package br.com.estudo_framework.cursoFramework;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursoFrameworkApplication {
+public class CursoFrameworkApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoFrameworkApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Projeto Spring sem Web");
 	}
 
 }
