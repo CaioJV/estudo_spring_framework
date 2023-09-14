4abfdbf5b894f3aa140940ba9ba4ec2d84ab19f8
administrador <administrador@eq20206299am.AMRJRJ.AD>
2023-09-14 17:59:00 -0300

    Modificação da classe main

--- cursoFramework/src/main/java/br/com/estudo_framework/cursoFramework/CursoFrameworkApplication.java
+++ cursoFramework/src/main/java/br/com/estudo_framework/cursoFramework/CursoFrameworkApplication.java
@@ -4,17 +4,21 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.estudo_framework.service.ConsumoApi;
+
 @SpringBootApplication
 public class CursoFrameworkApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoFrameworkApplication.class, args);
-		System.out.println("Branch Casa");
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Projeto Spring sem Web");
+		System.out.println("Primeiro projeto Spring sem Web");
+		ConsumoApi consumoAPI = new ConsumoApi();
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println("json");
 	}
 
 }
