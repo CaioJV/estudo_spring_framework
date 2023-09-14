e75a738290e3ac2ff62741fb0e94b87d9fac4636
CaioJV <88401108+CaioJV@users.noreply.github.com>
2023-09-13 22:13:44 -0300

    Comit no Branch Casa

--- cursoFramework/src/main/java/br/com/estudo_framework/cursoFramework/CursoFrameworkApplication.java
+++ cursoFramework/src/main/java/br/com/estudo_framework/cursoFramework/CursoFrameworkApplication.java
@@ -8,6 +8,7 @@ public class CursoFrameworkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoFrameworkApplication.class, args);
+		System.out.println("Branch Casa");
 	}
 
 }
