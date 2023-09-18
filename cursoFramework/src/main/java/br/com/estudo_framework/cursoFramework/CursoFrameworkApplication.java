c5cfed8fc8e3e65b4e107fa0e6d182d3a811e13c
CaioJV <88401108+CaioJV@users.noreply.github.com>
2023-09-17 23:28:47 -0300

    Adicionando o objeto dados das Temporadas e exibindo todos os episódios de todas as temporadas

--- cursoFramework/src/main/java/br/com/estudo_framework/cursoFramework/CursoFrameworkApplication.java
+++ cursoFramework/src/main/java/br/com/estudo_framework/cursoFramework/CursoFrameworkApplication.java
@@ -1,10 +1,15 @@
 package br.com.estudo_framework.cursoFramework;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.estudo_framework.cursoFramework.model.DadosEpisodio;
 import br.com.estudo_framework.cursoFramework.model.DadosSerie;
+import br.com.estudo_framework.cursoFramework.model.DadosTemporada;
 import br.com.estudo_framework.service.ConsumoApi;
 import br.com.estudo_framework.service.ConverterDados;
 
@@ -17,13 +22,22 @@ public class CursoFrameworkApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Primeiro projeto Spring sem Web");
+		List<DadosTemporada> listaTemporadas = new ArrayList<>();
 		ConsumoApi consumoAPI = new ConsumoApi();
 		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		System.out.println(json);
 		ConverterDados conversor = new ConverterDados();
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados.toString());
-	}
+		json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio.toString());
 
+		for (int i = 1; i <= dados.totalTemporadas(); i++) {
+			json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			listaTemporadas.add(dadosTemporada);
+		}
+		listaTemporadas.forEach(System.out::println);
+
+	}
 }
